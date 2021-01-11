6a1784a617623ffb5b46b4d989af5a8d712a5fa6
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-11 12:11:28 +0100

    Matching des couleurs

--- Files_proteges/header.php
+++ Files_proteges/header.php
@@ -1,6 +1,6 @@
 <!-- Navbar -->
 <div class="header">
-  <a href="#default" class="logo">myBocuse</a>
+  <a href="#default" class="logo">My.Bocuse</a>
   <div class="header-right">
     <a class="active" href="#login">Login</a>
     <a href="#contact">Contact</a>
