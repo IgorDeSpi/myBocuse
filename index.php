6fb29c30069de1425dc0266ca6887595ec5371ca
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-11 10:40:09 +0100

    Base commune d'index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myBocuse</title>
+    <link rel="icon" href="./img/favicon.ico" type="image/png" />
+</head>
+<body>
+
+<!-- Header -->
+
+
+
+
+<!-- Main -->
+
+
+
+
+<!-- Footer -->
+
+
+
+
+    
+</body>
+</html>
