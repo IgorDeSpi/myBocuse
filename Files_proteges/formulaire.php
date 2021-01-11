1fcc3451807bcbc67f3e4c0bbe04b2019a9f5eee
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-11 11:53:18 +0100

    Formulaire de connexion

---
+++ Files_proteges/formulaire.php
@@ -0,0 +1,29 @@
+<main>
+<H1 class="bocuse_title">Welcome to My.Bocuse</H1>
+
+<div class="main_container">
+     
+    
+
+    <form method="POST">
+        <h1 class="title">Login</h1>
+
+        <p>
+            <label for="email"><b>E-mail adress :</b></label>
+            <input type="email" name="email" placeholder="Enter a valid e-mail adress..."  />
+        </p>
+
+        <p>
+            <label for="pwd"><b>Passsword :</b></label>
+            <input type="password" name="pwd" placeholder="Enter your password..."  />
+        </p>
+
+        <p>
+            <input type="submit" value="Confirm" /> 
+        </p>
+
+
+
+    </form>
+</div>
+</main>
--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <title>myBocuse</title>
+
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet"> 
+    
+    <title>My.Bocuse</title>
+    
     <link rel="icon" href="./img/favicon.ico" type="image/png" />
 </head>
 <body>
@@ -16,7 +21,7 @@
 
 <!-- Main -->
 
-
+<?php include('./Files_proteges/formulaire.php'); ?>
 
 
 <!-- Footer -->
