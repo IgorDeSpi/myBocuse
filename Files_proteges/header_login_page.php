c65781a7f5705c3b445b5bea1d8690eb18f46d76
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-12 10:58:15 +0100

    Bouton log out et changement dans la nav bar

--- Files_proteges/header_login_page.php
+++ Files_proteges/header_login_page.php
@@ -1,10 +1,21 @@
+<!-- <header>
+    <nav class="navBar">
+        <ul class="menuNav">
+            <li><a href="">Bertrand</a></li>
+            <li><a href="">Photo</a></li>
+            <li><a href="logout.php">Disconnect</li></a>
+        </ul>
+    </nav>
+</header> -->
 
-    <header>
-        <nav class="navBar">
-            <ul class="menuNav">
-                <li><a href="">Bertrand</a></li>
-                <li><a href="">Photo</a></li>
-                <li><a href="logout.php">Disconnect</li></a>
-            </ul>
-        </nav>
-    </header>
+
+<header class="site-header">
+    <div class="header">
+        <a href="#default" class="logo zoom_logo"><img src="./logo.png" height=80px width=80px></a>
+        <div class="header-right">
+            <div class="zoom_login"><a href="#">Bertrand</a></div>
+            <div class="zoom_contact"><a href="#">Photo</a></div>
+            <div class="zoom_contact"><a href="./logout.php">Log out</a></div>
+        </div>
+    </div>
+</header>
--- logout.php
+++ logout.php
@@ -4,3 +4,4 @@ session_destroy();
 header("Location: ./index.php");
 
 ?>
+<a href="index.php">Retour à l'acceuil</a>
