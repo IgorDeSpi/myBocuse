4309082682958e893988502242cb1f57ad2c84d9
IgorDeSpi <igor.de.spirlet@hotmail.com>
2021-01-12 15:51:39 +0100

    Arrangement bouton + formulaire recette

--- Files_proteges/header_formulaire.php
+++ Files_proteges/header_formulaire.php
@@ -2,10 +2,10 @@
 <!-- Header -->
 <header class="site-header">
 <div class="header">
-<a href="#default" class="logo zoom_logo"><img src="./logo.png" height=80px width=80px></a>
+<a href="#default" class="logo zoom"><img src="./logo.png" height=80px width=80px></a>
   <div class="header-right">
-    <div class="zoom_login"><a class="active padding_box " href="#login">Login</a></div>
-    <div class="zoom_contact"><a class="padding_box " href="#contact">Contact</a></div>
+    <div class="zoom"><a class="active padding_box " href="#login">Login</a></div>
+    <div class="zoom"><a class="padding_box " href="#contact">Contact</a></div>
   </div>
 </div> 
 </header>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=my.bocuse;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=my.bocuse;charset=utf8', 'root', '');
 }
 catch (Exception $e)
 {
