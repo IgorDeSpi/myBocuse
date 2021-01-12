86efabb070c2f992b9afa647f1c12341303b3c0a
bruxellesflorian <florianbertchi@gmail.com>
2021-01-12 12:22:49 +0100

    Ajout de variable sur le loginPage

--- Files_proteges/header_login_page.php
+++ Files_proteges/header_login_page.php
@@ -8,12 +8,11 @@
     </nav>
 </header> -->
 
-
 <header class="site-header">
     <div class="header">
         <a href="#default" class="logo zoom_logo"><img src="./logo.png" height=80px width=80px></a>
         <div class="header-right">
-            <div class="zoom_login"><a href="#">Bertrand</a></div>
+            <div class="zoom_login"><a href="#"><?php echo $_SESSION['email']; ?></a></div>
             <div class="zoom_contact"><a href="#">Photo</a></div>
             <div class="zoom_contact"><a href="./logout.php">Log out</a></div>
         </div>
