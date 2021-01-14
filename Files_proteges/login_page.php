04933ccdb9307b56c9131e09af9a5e3a85deff33
bruxellesflorian <florianbertchi@gmail.com>
2021-01-14 09:11:43 +0100

    ajout de fonctionnalité sur le pointage

--- Files_proteges/login_page.php
+++ Files_proteges/login_page.php
@@ -18,32 +18,40 @@
     </section>
 
     <div class="flex">
-                <div class="boxRecette">
+        <?php include('./Files_proteges/calendrierRecette.php'); ?>
+        <div>   
 
-                <?php
-                $reponse = $bdd->query('SELECT * FROM recette ORDER BY dateR');
-                while($donnees = $reponse->fetch())
-                {
-                ?>
-                <div class="boxTitre">
-                    
-                <div ><?php echo $donnees['dateR'] ?></div>
-                <div ><?php echo $donnees['titre'] ?></div>
-                <div ><?php echo $donnees['contenu'] ?></div>
+            <form action="" method="post">  
+            <input  type="date" name="dateP" value=<?php echo $jour; ?> required>
+            <input type="submit"  value="recherche"required>
+            </form>
+            
+        </div>
+            <div class="recetteDJ">
+                    RECETTE DU JOUR
+                <div>
+                <?php 
+            // if(isset($_POST["dateP"])){ 
+            //     $dateDemand = $_POST["dateP"];
+            //     print_r($dateDemand);
+            //     echo'<br>';
+            //     $recherche = $bdd->prepare('SELECT * FROM recette WHERE dateR');
+            //     $recherche->execution(array($dateDemand));
+
+            //     while($donnee = $recherche->fetch()){
+
+            //        echo $donnee['dateR'],'<br>';
+            //        echo $donnee['contenu'];
+                
+            //     }
+            //     $reponse->closeCursor();
+            // }
+            ?>
                 </div>
-                <?php
-                }         
-                $reponse->closeCursor(); // Termine le traitement de la requête
-                ?>
-   
-   
-</div>
-<div class="recetteDJ">RECETTE DU JOur</div>
             </div>
-        </div> 
-            </div> 
-
-<?php include('./Files_proteges/creation_recette.php'); ?>
+        
+    </div>
+    <?php include('./Files_proteges/creation_recette.php'); ?>
 
 
 
