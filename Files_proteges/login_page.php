c2b5685cdfe89294ac5291d276b09cb60fca9c14
bruxellesflorian <florianbertchi@gmail.com>
2021-01-14 17:30:35 +0100

    Mise en place d'une page de suivie coté CHEF

--- Files_proteges/login_page.php
+++ Files_proteges/login_page.php
@@ -3,9 +3,11 @@
     <div class="pointage1">
         <?php   
         // echo 'Bienvenu ', $_SESSION['statut'],' ',$_SESSION['prenom'],' ',$_SESSION['nom'];
-        if($_SESSION['statut'] === 'chef'){
+        if($_SESSION['statut'] == 'Chef'){
             echo 'Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' ! ';  
-        } else if($_SESSION['statut'] === 'learner'){
+            include('./Files_proteges/regardPointage.php');
+
+        } else if($_SESSION['statut'] === 'Learner'){
             echo 'Bonjour ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' ! ';   // Peut afficher des choses en plus
             include('./Files_proteges/pointage.php');
         }
---
+++ Files_proteges/regardPointage.php
@@ -0,0 +1,29 @@
+<h2>Pointage des élèves</h2>
+<?php
+$quiAPointe = $bdd->query('SELECT * FROM pointage ORDER BY jour');
+while($donnees = $quiAPointe->fetch())
+{
+    $nom = $donnees['nom'];
+    $prenom = $donnees['prenom'];
+    $start = $donnees['startTime'];
+    $fin = $donnees['finish'];
+    $date = $donnees['jour'];
+}
+?>
+<table class="tablePersonne">
+    <tr class="bolder">
+        <td class="td"> Nom</td>
+        <td class="td"> Prenom</td>
+        <td class="td"> Date</td>
+        <td class="td"> Start</td>
+        <td class="td"> Fin</td>
+    </tr>
+    <tr>
+        <td class="td"><?php echo '<br>',$nom ?></td>
+        <td class="td"><?php echo '<br>',$prenom ?></td>
+        <td class="td"><?php echo '<br>',$date ?></td>
+        <td class="td"><?php echo '<br>',$start ?></td>
+        <td class="td"><?php echo '<br>',$fin ?></td>
+    </tr>
+
+</table>
