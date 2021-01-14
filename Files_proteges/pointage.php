414685e0713d87e857959ce7961d3307335c9926
bruxellesflorian <florianbertchi@gmail.com>
2021-01-14 18:58:45 +0100

    JE PUSH TOUT

--- Files_proteges/pointage.php
+++ Files_proteges/pointage.php
@@ -1,72 +1,70 @@
 <?php
 
+
  $heureDepart = "9:00";
  $heureDeFin = "17:00";
  $matin = "";
  $soir ="";
-
+ $stopMatin = false;
+ $stopSoir = false;
 
  $id = $_SESSION['id'];
  $jour = date('Y-m-d');
-
-    // $PointageMatin = $bdd->prepare("SELECT `id`,`jour`,`startTime`,`finish` FROM `pointage` WHERE id=?");
-    $PointageMatin = $bdd->prepare("SELECT `jour`,`startTime` FROM `pointage` WHERE user=? AND jour=?");
-    $PointageMatin->execute(array($id,date('Y-m-d')));
-    $data = $PointageMatin->fetch();
-    $PointageMatin->closeCursor();
-          if(empty($data)){
-              if(isset($_POST['pMatin'])){
-                //   header("Location: index.php");
+    
+    $PointageMatin = $bdd->query("SELECT `id`,`jour`,`startTime`,`finish` FROM `pointage` WHERE $id");
+    while($donnee = $PointageMatin->fetch())
+    {
+        if($donnee['jour'] == $jour ){
+            header("index.php");
+            $matin = "pointMatin"; 
+            $heureDepart = $donnee['startTime'];
+            $stopMatin = true; 
+        }
+    }
+    $PointageMatin->closeCursor(); 
+    // $bdd = new PDO("mysql:host=localhost;dbname=my.bocuse;charset=utf8", "root", "root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        if($stopMatin !== true){
+            if(isset($_GET['pMatin'])){
                 $timestamp = date('h:i:s'); 
-
-                  echo "Pointé à $timestamp ";
-                  $addTime = $bdd->prepare("INSERT INTO `pointage` (`id`, `user`, `jour`, `startTime`, `finish`,`nom`,`prenom` ) VALUES (NULL, $id, CURRENT_DATE, CURRENT_TIME, '','$_SESSION['nom']','$_SESSION['prenom']')");
-                  $addTime->execute(array(NULL, $id, 'CURRENT_DATE', 'CURRENT_TIME', '',$_SESSION['nom'],$_SESSION['prenom']));
-                  $heureDepart = $timestamp;
-              };  
-          }else{
-            $matin = "pointMatin";
-          }
-          $PointageSoir = $bdd->prepare("SELECT `finish` FROM `pointage` WHERE user=? AND jour=?");
-          $PointageSoir->execute(array($id,date('Y-m-d')));
-          $data2 = $PointageSoir->fetch();
-          $PointageSoir->closeCursor();            
-            // if(empty($data2)=== "00:00:00"){
-                if(isset($_POST['pSoir'])){
-                        $bdd->exec("UPDATE `pointage` SET `finish`= CURRENT_TIME WHERE $id ");
-                        $PointageSoir = $bdd->query("SELECT `id`,`jour`,`startTime`,`finish` FROM `pointage` WHERE $id");
-                        while($donnee = $PointageSoir->fetch())
-                        {
-                                if($donnee['jour'] == $jour ){
-                                    $stopSoir = true; 
-                                    $heureDeFin = $donnee['finish'] ;
-                                    header("index.php"); 
-                                    $soir = "pointSoir";                         
-                                }
-                        $PointageSoir->closeCursor(); 
-                        }
-                }
-            // }
-
-
-
+                $addTime = $bdd->prepare("INSERT INTO `pointage` (`id`, `user`, `jour`, `startTime`, `finish`) VALUES (NULL, $id, CURRENT_DATE, CURRENT_TIME, '')");
+                $addTime->execute(array(NULL, $id, 'CURRENT_DATE', 'CURRENT_TIME', ''));             
+            };     
+        }   
+        if($stopSoir === false){
+        if(isset($_GET['pSoir'])){
+                $bdd->exec("UPDATE `pointage` SET `finish`= CURRENT_TIME WHERE $id ");
+                $PointageMatin = $bdd->query("SELECT `id`,`jour`,`startTime`,`finish` FROM `pointage` WHERE $id");
+                while($donnee = $PointageMatin->fetch())
+                {
+                        if($donnee['jour'] == $jour ){
+                            $stopSoir = true; 
+                            $heureDeFin = $donnee['finish'] ;
+                            header("index.php"); 
+                            $soir = "pointSoir";                         
+                    }
+        }
+        $PointageMatin->closeCursor(); 
+    }
+}
     ?>
 <section class="pointage">
-    <div class="pointage1">
-        <h2>Pointage</h2>
-        <div class="pointage2">
-            <div class="heurepointage">
-                <h3>Start</h3>
-                <form action="" method="post">
+        <div class="pointage1">
+            <h2>Pointage</h2>
+            <div class="pointage2">
+                <div class="heurepointage">
+                    <h3>Start</h3>
+                    <form action="" method="get">
                     <button id="bouton_pointage" name="pMatin"><?php echo $heureDepart; ?></button>
-            </div>
-            <div class="heurepointage">
-                <h3>End</h3>
-                <button id="bouton_pointage" class="boutonSoir" name="pSoir"><?php echo $heureDeFin;?></button>
-                </form>
+                </div>
+                <div class="heurepointage">
+                    <h3>End</h3>
+                    <button id="bouton_pointage" class="boutonSoir" name="pSoir"><?php echo $heureDeFin;?></button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<div class="pointMatin"><?php echo $matin; ?></div>
-<div class="pointSoir"><?php echo $soir; ?></div>
+    </section>
+    <div class="pointMatin"><?php echo $matin; ?></div>
+    <div class="pointSoir"><?php echo $soir; ?></div>
+    
+
